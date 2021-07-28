ffb3b3548af8e8d76fbfb4d2dc587786517ee722
JalenMcC <76828457+JalenMcC@users.noreply.github.com>
2021-07-27 21:00:34 -0400

    Made it look somewhat nicer, removed other useless stuff

--- main.js
+++ main.js
@@ -1,52 +1,3 @@
-/*
-//Example Stuff
-let car = 'Mosh';
-car = 5;
-console.log(car);
-const truck = true;
-console.log(truck);
-
-let person = {
-    name: 'Mosh',
-    age: 30
-};
-console.log(person);
-
-person.name = 'John';
-console.log(person);
-
-person['name'] = 'Mary';
-console.log(person.name);
-
-let selectedColors = ['red', 'blue'];
-console.log(selectedColors);
-
-selectedColors[2] = 1;
-console.log(selectedColors);
-console.log(selectedColors.length);
-
-function greet(card, word) {
-    console.log('Hello ' + card + ' ' + word);
-}
-
-greet('John', 'Mary');
-
-function square(number) {
-    return number * number;
-}
-
-console.log(square(4));
-
-//Comments still work like this
-
-if (car === 'Jane') {
-    alert('Its fuckin Jane!');
-}
-else {
-    alert('Oh no!');
-}
-*/
-
 //Part 1
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
@@ -78,8 +29,6 @@ function result() {
     newStory = newStory.replace(':inserty:', yItem);
     newStory = newStory.replace(':insertz:', zItem);
     newStory = newStory.replace(':insertx:', xItem);
-    //End Part 3 Written Stuff
-
 
   if(customName.value !== '') {
     let name = customName.value;
